f2be9f782540904240ea8420738505467004056d
rexim <reximkut@gmail.com>
2021-05-05 03:42:07 +0700

    Implement the classical square bouncing off of the borders

--- game.rs
+++ game.rs
@@ -8,8 +8,8 @@ fn panic(_panic: &PanicInfo<'_>) -> ! {
     loop {}
 }
 
-const WIDTH: usize = 100;
-const HEIGHT: usize = 100;
+const WIDTH: usize = 800;
+const HEIGHT: usize = 600;
 
 #[derive(Clone, Copy)]
 #[repr(C)]
@@ -27,6 +27,7 @@ impl Pixel {
 const RED: Pixel   = Pixel::rgba(0xFF, 0, 0, 0xFF);
 const GREEN: Pixel = Pixel::rgba(0, 0xFF, 0, 0xFF);
 const BLUE: Pixel  = Pixel::rgba(0, 0, 0xFF, 0xFF);
+const BACKGROUND: Pixel = Pixel::rgba(18, 18, 18, 0xFF);
 
 #[derive(Copy, Clone)]
 struct RGBA {
@@ -62,6 +63,21 @@ impl Display {
             }
         }
     }
+
+    fn fill_rect(&mut self, x0: i32, y0: i32, w: i32, h: i32, pixel: Pixel) {
+        assert!(WIDTH > 0);
+        assert!(HEIGHT > 0);
+        let x1 = x0.clamp(0, (WIDTH - 1) as i32) as usize;
+        let x2 = (x0 + w - 1).clamp(0, (WIDTH - 1) as i32) as usize;
+        let y1 = y0.clamp(0, (HEIGHT - 1) as i32) as usize;
+        let y2 = (y0 + h - 1).clamp(0, (HEIGHT - 1) as i32) as usize;
+
+        for y in y1..=y2 {
+            for x in x1..=x2 {
+                self.pixels[y][x] = pixel;
+            }
+        }
+    }
 }
 
 type Seconds = f32;
@@ -72,6 +88,10 @@ static mut DISPLAY: Display = Display {
 
 pub struct State {
     time: Seconds,
+    x: i32,
+    y: i32,
+    dx: i32,
+    dy: i32,
 }
 
 impl State {
@@ -81,7 +101,11 @@ impl State {
 }
 
 static mut STATE: State = State {
-    time: 0.0
+    time: 0.0,
+    x: 10,
+    y: 10,
+    dx: 1,
+    dy: 1,
 };
 
 #[no_mangle]
@@ -105,19 +129,23 @@ pub fn get_display() -> *mut Display {
 pub fn next_frame(dt: Seconds) {
     unsafe {
         STATE.update(dt);
-        for y in 0..HEIGHT {
-            for x in 0..WIDTH {
-                let ix = x as f32 / WIDTH as f32;
-                let iy = y as f32 / HEIGHT as f32;
-                DISPLAY.pixels[y][x] =
-                    RGBA::new(
-                        (js_sin(STATE.time + ix) + 1.0) * 0.5,
-                        (js_cos(STATE.time + iy) + 1.0) * 0.5,
-                        (js_cos(STATE.time + iy + ix) + 1.0) * 0.5,
-                        1.0)
-                    .to_pixel();
-            }
+        DISPLAY.fill(BACKGROUND);
+
+        const SPEED: i32 = 16;
+        const RECT_WIDTH: i32 = 100;
+        const RECT_HEIGHT: i32 = 100;
+
+        if STATE.x < 0 || STATE.x + RECT_WIDTH > WIDTH as i32 {
+            STATE.dx = -STATE.dx;
         }
+
+        if STATE.y < 0 || STATE.y + RECT_HEIGHT > HEIGHT as i32 {
+            STATE.dy = -STATE.dy;
+        }
+
+        STATE.x += STATE.dx * SPEED;
+        STATE.y += STATE.dy * SPEED;
+        DISPLAY.fill_rect(STATE.x, STATE.y, RECT_WIDTH, RECT_HEIGHT, RED);
     }
 }
 
